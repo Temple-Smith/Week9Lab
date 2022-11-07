a061ee9ff174178bc91fe71daf6d455f3ba75df7
Drew- <Drew-@MSI>
2022-11-06 23:59:44 -0700

    Add structure from Week 7/8 to do JPA lab for Week 9

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -9,6 +9,15 @@ package dataaccess;
  *
  * @author Drew-
  */
+package dataaccess;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DBUtil {
+    public static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("UserPU");
     
+    public static EntityManagerFactory getEmFactory() {
+        return EMF;
+    }
 }
